6a1ef7be1bb122d95beb5bc1db56b8a9a1ff5867
stylehaullong <stylehaullong@users.noreply.github.com>
2016-10-10 09:52:35 -0700

    Add files via upload

---
+++ client/components/bins/bins_main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BinsMain extends Component {
+    render () {
+        return (
+            <div>BinsMain</div>
+        );
+    }
+}
+
+
+
+export default BinsMain;
